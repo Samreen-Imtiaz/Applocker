5cc9b7a4b5dc631f93119698f9c694f4ad79adb1
Samreen-Imtiaz <syeda_samreenimtiaz@outlook.com>
2016-09-20 20:49:36 +0500

    UserManual_commit

---
+++ app/src/main/java/test/applocker/com/applocker/User_Manual.java
@@ -0,0 +1,102 @@
+package test.applocker.com.applocker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
+/**
+ * Created by samreen on 9/18/2016.
+ */
+
+
+public class User_Manual extends Activity {
+
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+    private ViewFlipper mViewFlipper;
+    private Animation.AnimationListener mAnimationListener;
+    private Context mContext;
+    Button contnue;
+
+    @SuppressWarnings("deprecation")
+    private final GestureDetector detector = new GestureDetector(new SwipeGestureDetector());
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial_slide);
+        mContext = this;
+        mViewFlipper = (ViewFlipper) this.findViewById(R.id.view_flipper);
+        mViewFlipper.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(final View view, final MotionEvent event) {
+                detector.onTouchEvent(event);
+                return true;
+            }
+        });
+
+
+        //animation listener
+        mAnimationListener = new Animation.AnimationListener() {
+            public void onAnimationStart(Animation animation) {
+                //animation started event
+            }
+
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            public void onAnimationEnd(Animation animation) {
+                //TODO animation stopped event
+            }
+        };
+        contnue = (Button) findViewById(R.id.button3);
+        contnue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(User_Manual.this, User_Profile.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
+    class SwipeGestureDetector extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                // right to left swipe
+                if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(mContext, R.anim.left_in));
+                    mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(mContext, R.anim.left_out));
+                    // controlling animation
+                    mViewFlipper.getInAnimation().setAnimationListener(mAnimationListener);
+                    mViewFlipper.showNext();
+                    return true;
+                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(mContext, R.anim.right_in));
+                    mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(mContext,R.anim.right_out));
+                    // controlling animation
+                    mViewFlipper.getInAnimation().setAnimationListener(mAnimationListener);
+                    mViewFlipper.showPrevious();
+                    return true;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return false;
+        }
+    }
+
+
+}
